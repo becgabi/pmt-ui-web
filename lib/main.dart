d4bdc3109e295636d14803637077e44a31873c5d
Gábor Beckl <becgabi@gmail.com>
2022-05-25 14:42:13 +0200

    Fix location of main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 
-import 'app.dart';
+import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
